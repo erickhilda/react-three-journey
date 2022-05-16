fb5fa0c86cb86ed565b20a4974303bb7de28be03
Erick Hilda <erick.hilda@ajaib.co.id>
2022-05-16 13:36:42 +0700

    feat: add animation

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
+
 // Size
 const sizes = {
   width: 800,
@@ -11,43 +13,17 @@ const sizes = {
 
 // Scene
 const scene = new THREE.Scene();
-const axesHelper = new THREE.AxesHelper(2);
-scene.add(axesHelper);
 
-// Object
-/**
- * Objects
- */
- const group = new THREE.Group()
- group.scale.y = 2
- group.rotation.y = 0.2
- scene.add(group)
- 
- const cube1 = new THREE.Mesh(
-     new THREE.BoxGeometry(1, 1, 1),
-     new THREE.MeshBasicMaterial({ color: 0xff0000 })
- )
- cube1.position.x = - 1.5
- group.add(cube1)
- 
- const cube2 = new THREE.Mesh(
-     new THREE.BoxGeometry(1, 1, 1),
-     new THREE.MeshBasicMaterial({ color: 0xff0000 })
- )
- cube2.position.x = 0
- group.add(cube2)
- 
- const cube3 = new THREE.Mesh(
-     new THREE.BoxGeometry(1, 1, 1),
-     new THREE.MeshBasicMaterial({ color: 0xff0000 })
- )
- cube3.position.x = 1.5
- group.add(cube3)
+// Objects
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.lookAt(cube2.position);
+camera.lookAt(cube.position);
 scene.add(camera);
 
 // Renderer
@@ -55,4 +31,17 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+/**
+ * Animate
+ */
+gsap.to(cube.position, { duration: 1, delay: 1, x: 2 });
+
+const tick = () => {
+  //   cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
