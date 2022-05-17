79387a8e353c282580091758f9a6bb56a0b80cf2
Erick Hilda <erick.hilda@ajaib.co.id>
2022-05-17 07:36:21 +0700

    chore: Initiate project using next and r3f

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+// const withTM = require('next-transpile-modules')(['three'])
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
